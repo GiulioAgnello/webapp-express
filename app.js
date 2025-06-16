99d800c88301c3638d4a98d88d9f915aa11c2973
agnellogiulio4@gmail.com <agnellogiulio4@gmail.com>
2025-06-16 15:38:40 +0200

    baseConfig

--- app.js
+++ app.js
@@ -1,9 +1,16 @@
+// Import and config
 const express = require("express");
+require("dotenv").config();
+
+// config app and kays
 const app = express();
 const port = process.env.APP_PORT;
+const urlHost = process.env.APP_HOST;
 
+// route
 app.get("/", (req, res) => res.send("Hello World!"));
 
+// listening
 app.listen(port, () =>
-  console.log(`Example app listening at http://localhost:${port}`)
+  console.log(`Example app listening at ${urlHost}${port}`)
 );
